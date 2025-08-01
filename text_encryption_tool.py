82599e6be1f31e661fdf71247845e6e6906f15dc
sridh4r <sri2007vds@gmail.com>
2025-08-01 23:59:14 +0530

    Improved key validation: allow user to re-enter key on failure, fixed typos, and enhanced output formatting

--- text_encryption_tool.py
+++ text_encryption_tool.py
@@ -2,20 +2,26 @@ import string
 import random
 
 def validKey(key):
-    if key==None or key=="":
-        return True
-    elif len(key)!=95:
-        return False
-    return True
+    if not key:
+        return key
+    elif len(key)!=95 or len(set(key))!=95:
+        print("Your key is invalid make sure it contain 95 unique characters")
+        x=input("do you have a valid Key (Y/N) if no the program will produce a new key: ").upper()
+        if x=='Y':
+            key=input("Enter your valid key: ")
+            return validKey(key)
+        else:
+            key=None
+    return key
 
 
 def printLine():
-    print("*************************")
+    print("-----------------------------------------------")
 
 def encrypt(key=None):
     value=string.ascii_letters+string.digits+string.punctuation+" "
     value_char=list(value)
-    if key==None or key=="":
+    if not key:
         key=value_char.copy()
         random.shuffle(key)
     else:
@@ -26,14 +32,14 @@ def encrypt(key=None):
         map[value_char[i]]=key[i]
     return map , key
 
-def encrypt_message(message,map):
+def encrypt_message(message,cipher_map):
     encrypted_message=""
     for ch in message:
-        encrypted_message+=map[ch]
+        encrypted_message+=cipher_map[ch]
     return encrypted_message
 
-def decrypt_message(message,map):
-    map_inv={v:k for k , v in map.items()}
+def decrypt_message(message,cipher_map):
+    map_inv={v:k for k , v in cipher_map.items()}
     original_message=""
     for ch in message:
         original_message+=map_inv[ch]
@@ -43,36 +49,31 @@ def decrypt_message(message,map):
 
 def main():
     printLine()
+    print("- - - - - - - - Text Encryption- - - - - - - - ")
+    printLine()
     key=input("Enter your key if you don't have one press enter: ")
-    if not validKey(key):
-        x=int(input("The key you have been provided is not valid press 1 to create new one press 2 to exit: "))
-        if x==1:
-            key=None
-        else:
-            print("Program exit")
-            printLine()
-            return
+    key=validKey(key)
+        
        
-    map,key=encrypt(key)
+    cipher_map,key=encrypt(key)
     while True:
         try:
             printLine()
-            n = int(input("Whatdo you want to do with the meaage 1 -> encrypt  , 2 -> decrypt , 3 -> exit :"))
+            n = int(input("What do you want to do with the meaage 1 -> encrypt  , 2 -> decrypt , 3 -> exit: "))
         except ValueError:
             print("Please enter a valid number (1, 2, or 3).")
-            printLine()
             continue
         if n==1:
             message=input("Enter your Message: ")
-            print(f"""Encrypted message is :"{encrypt_message(message,map)}" \ncopy for decryption""")
+            print(f"Encrypted message is: {encrypt_message(message,cipher_map)}")
             
         elif n==2:
-            message=input("Enter your Message: ")
-            print(f"""original message is :"{decrypt_message(message,map)}" \ncopy for encryption""")
+            message=input("Enter your decrypt message: ")
+            print(f"original message is: {decrypt_message(message,cipher_map)}")
             
 
         elif n==3:
-            print(f"""here is your key for future reference  '{''.join(key)}' Note the key did't include the first and last single quotes """)
+            print(f"here is your key for future reference:  {''.join(key)}")
             break
             
         else:
