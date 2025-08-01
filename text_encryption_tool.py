10265a011e545b2d4c515a39fc6197266594340d
Sridhar <sri2007vds@gmail.com>
2025-08-01 22:42:49 +0530

    Add files via upload

---
+++ text_encryption_tool.py
@@ -0,0 +1,68 @@
+import string
+import random
+
+def printLine():
+    print("*************************")
+
+def encrypt(key=None):
+    value=string.ascii_letters+string.digits+string.punctuation+" "
+    value_char=list(value)
+    if key==None or key=="":
+        key=value_char.copy()
+        random.shuffle(key)
+    else:
+        key=list(key)
+    
+    map={}
+    for i in range(len(value_char)):
+        map[value_char[i]]=key[i]
+    return map , key
+
+def encrypt_message(message,map):
+    encrypted_message=""
+    for ch in message:
+        encrypted_message+=map[ch]
+    return encrypted_message
+
+def decrypt_message(message,map):
+    map_inv={v:k for k , v in map.items()}
+    original_message=""
+    for ch in message:
+        original_message+=map_inv[ch]
+    return original_message
+
+
+
+def main():
+    printLine()
+    key=input("Enter your key if you don't have one press enter: ")
+    map,key=encrypt(key)
+    while True:
+        try:
+            printLine()
+            n = int(input("Whatdo you want to do with the meaage 1 -> encrypt  , 2 -> decrypt , 3 -> exit :"))
+        except ValueError:
+            print("Please enter a valid number (1, 2, or 3).")
+            printLine()
+            continue
+        if n==1:
+            message=input("Enter your Message: ")
+            print(f"""Encrypted message is :"{encrypt_message(message,map)}" \ncopy for decryption""")
+            
+        elif n==2:
+            message=input("Enter your Message: ")
+            print(f"""original message is :"{decrypt_message(message,map)}" \ncopy for encryption""")
+            
+
+        elif n==3:
+            print(f"""here is your key for future reference  '{''.join(key)}' Note the key did't include the first and last single quotes """)
+            break
+            
+        else:
+            print("Please enter a valid input")
+            
+
+
+
+if __name__=='__main__':
+    main()
